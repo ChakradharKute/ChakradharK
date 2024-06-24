fcbb43b6f47fc2d573bad3ff58c7696cf5103f70
Chakradhar <chakradharkute1199@gmail.com>
2024-06-24 06:45:18 +0530

    Make some Changes

--- src/main/java/com/book/controller/BookController.java
+++ src/main/java/com/book/controller/BookController.java
@@ -19,6 +19,11 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+    
+    @GetMapping()
+    public String saySorry() {
+    return "Sorry";
+    }
 
   @PostMapping("/add")
   public Book addBook(@RequestBody Book book)
